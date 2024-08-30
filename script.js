3277dc06dd8828bca973601136dbb653aa97ef0b
Franco Galfre <francogalfre2007@gmail.com>
2024-08-29 22:37:05 -0300

    feat: update card using querySelector

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 console.log("Welcome to the JS E-Commerce");
 console.log("Yes yes! Here we use Javascript 😎");
+
+const dibuMartinezName = "Emiliano Martinez";
+const dibuMartinezImage =
+  "https://storage.googleapis.com/diariodemocracia/news/dibu-martinez-aston-villa-Nottingham-premier-league-inglaterra-1-226cf5.jpg";
+
+const $title = document.querySelector("h5");
+const $image = document.querySelector(".card img");
+const $button = document.querySelector(".card button");
+
+$title.innerText = dibuMartinezName;
+$image.src = dibuMartinezImage;
+
+$button.addEventListener("click", () => {
+  alert(`Hola ${dibuMartinezName}, bienvenido a mi tienda!`);
+});
