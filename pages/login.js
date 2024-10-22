9e9677f5f0de06e58c410d856a6424c781c0b974
Franco Galfre <francogalfre2007@gmail.com>
2024-10-22 19:47:46 -0300

    feat: 🔐 Guardar email en localStorage y redirigir si el usuario está logueado

---
+++ pages/login.js
@@ -0,0 +1,28 @@
+import { user } from "../utils/user.js";
+
+const form = document.querySelector("#form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+
+  console.log("Email: ", email);
+  console.log("Password: ", password);
+
+  if (user.email === email && user.password === password) {
+    localStorage.setItem("userEmail", email);
+    window.location.href = "../index.html";
+  } else {
+    alert("Usuario incorrecto");
+  }
+});
+
+const userEmail = localStorage.getItem("userEmail");
+
+if (userEmail) {
+  window.location.href = "../index.html";
+}
+
+console.log(user);
---
+++ utils/user.js
@@ -0,0 +1,4 @@
+export const user = {
+  email: "admin@gmail.com",
+  password: "admin1234",
+};
