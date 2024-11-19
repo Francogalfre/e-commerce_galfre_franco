74da18f7058ee5d56be9d085115dcf173362fffa
Franco Galfre <francogalfre2007@gmail.com>
2024-11-19 18:57:57 -0300

    style: ajustando los estilos del carrito 🛒✨

--- pages/cart.js
+++ pages/cart.js
@@ -91,13 +91,18 @@ if (cartData) {
     card.classList.add("cart-item");
 
     card.innerHTML = `
-        <img src="${item.product.image}" alt="${item.product.title}" class="cart-item-image">
-        <div class="cart-item-details">
-          <h3 class="cart-item-title">${item.product.title}</h3>
-          <p class="cart-item-description">${item.product.description.slice(0, 200)}</p>
-          <p class="cart-item-quantity">Cantidad: ${item.quantity}</p>
-          <p class="cart-item-price">Precio Total: $ ${(item.product.price * item.quantity).toFixed(2)}</p>
-          <button class="btn btn-danger" onclick="removeProduct(${item.product.id})">Eliminar</button>
+        <img src="${item.product.image}" alt="${item.product.title}" class="cart-item-image img-fluid rounded me-3">
+        <div class="cart-item-details d-flex flex-column justify-content-between w-100">
+          <h3 class="cart-item-title fs-2 fw-bold text-primary mb-2">${item.product.title}</h3>
+          <p class="cart-item-description text-muted mb-2">${item.product.description.slice(0, 200)}</p>
+          <div class="d-flex justify-content-start align-items-center mb-3" style="gap: 10px;">
+            <p class="cart-item-price text-success fs-6 mb-0">Precio: $ ${item.product.price}</p>
+            <p class="cart-item-quantity mb-0"><strong>Cantidad:</strong> ${item.quantity}</p>
+          </div>
+          <p class="cart-item-total-price fs-5 fw-bold text-danger mb-3">Precio Total: $ ${(
+            item.product.price * item.quantity
+          ).toFixed(2)}</p>
+          <button class="btn btn-danger btn-sm mt-2 p-2" onclick="removeProduct(${item.product.id})">Eliminar</button>
         </div>
     `;
 
